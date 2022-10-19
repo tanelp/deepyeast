0777ae25fcda266ab56735e79e18e38ef7893bb9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 12:17:10 +0000

    Adding tarfile member sanitization to extractall()

--- deepyeast/dataset.py
+++ deepyeast/dataset.py
@@ -41,7 +41,26 @@ def get_file(fname, url, md5_hash, extract=False):
         if not os.path.exists(datadir):
             print('Extracting archive...')
             with tarfile.open(fpath, 'r:gz') as archive:
-                archive.extractall(datadir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(archive, datadir)
         
     return fpath
 
